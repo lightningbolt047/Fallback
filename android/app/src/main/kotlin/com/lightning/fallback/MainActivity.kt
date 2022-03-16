56110be7e3b30c28cc8d984398a76fab240bbdea
Sashank <sashankvisweshwaran@gmail.com>
2022-03-16 13:29:59 +0400

    [Backup] Attempt to add platform method

--- android/app/src/main/kotlin/com/lightning/fallback/MainActivity.kt
+++ android/app/src/main/kotlin/com/lightning/fallback/MainActivity.kt
@@ -1,6 +1,39 @@
 package com.lightning.fallback
 
+import android.os.Environment
+import android.provider.MediaStore
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.io.FileWriter
 
 class MainActivity: FlutterFragmentActivity() {
+    private val channelName="com.lightning.fallback"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger,channelName).setMethodCallHandler {
+            call, result ->
+                if(call.method=="saveBackupToDownloads"){
+                    try{
+                        saveBackupToDownloads(call.arguments(),result)
+//                        result.success("0")
+                    }catch (e: Exception){
+//                        result.error("1","Failed","Failed to write file")
+                    }
+                }else{
+                    result.notImplemented();
+                }
+
+        }
+    }
+
+    private fun saveBackupToDownloads(path: String?,result: MethodChannel.Result) {
+        val path=Environment.DIRECTORY_DOWNLOADS
+        MediaStore.Downloads.
+        val file: FileWriter = FileWriter(path)
+    }
+
+
+
 }
--- lib/services/local_backup_service.dart
+++ lib/services/local_backup_service.dart
@@ -3,6 +3,7 @@ import 'package:aes256gcm/aes256gcm.dart';
 import 'package:fallback/services/secure_storage.dart';
 import 'package:path/path.dart' as path;
 import 'dart:io';
+import 'package:flutter/services.dart';
 
 class LocalBackupService{
   final SecureStorage secureStorage;
@@ -13,8 +14,12 @@ class LocalBackupService{
       Map<String,dynamic> keys=await secureStorage.readKeys();
       String keysEncoded=jsonEncode(keys);
       String encrypted=await Aes256Gcm.encrypt(keysEncoded, (await secureStorage.readEncryptionPassword())!);
-      File file=File(path.join(directoryPath,"Fallback-backup-${DateTime.now().millisecondsSinceEpoch}.fbcrypt"));
-      await file.writeAsString(encrypted);
+      // File file=File(path.join(directoryPath,"Fallback-backup-${DateTime.now().millisecondsSinceEpoch}.fbcrypt"));
+      // await file.writeAsString(encrypted);
+      const channel=MethodChannel('com.lightning.fallback');
+      channel.invokeMethod('saveBackupToDownloads',"Fallback-backup-${DateTime.now().millisecondsSinceEpoch}.fbcrypt");
+
+
     }catch(e){
       print(e);
       return Future.error(e);
